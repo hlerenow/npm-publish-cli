853bbf0bb6b0a62d3dc8056c649c28c64fbaf1ee
lei.he <595806119@qq.com>
2019-05-06 18:57:45 +0800

    增加前端单元测试以及覆盖率报告生成,以及单元测试结果输出

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
-const webpack = require('./build/webpack.dev');
-delete webpack.entry;
-delete webpack.output;
+const webpack = require('./build/webpack.test');
+const path = require('path');
+
 module.exports = function(config) {
     config.set({
         frameworks: ['mocha', 'chai'],
@@ -11,27 +11,50 @@ module.exports = function(config) {
         preprocessors: {
             'test/unit/**/*.test.js': ['webpack']
         },
-        reporters: ['progress'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: ['Chrome'],
         captureTimeout: 60000,
-        singleRun: false
-        // client: {
-        //     mocha: {
-        //         // change Karma's debug.html to the mocha web reporter
-        //         reporter: 'html',
-        //         reporterOption: {
-        //             reportDir: './testReport/unit'
-        //         },
-        //         require: '@babel/register'
-        //     }
-        // }
+        reporters: ['html', 'coverage-istanbul'],
+        // any of these options are valid: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
+        coverageIstanbulReporter: {
+            // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
+            reports: ['html', 'lcovonly', 'text-summary'],
+            // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
+            dir: path.join(__dirname, './testReport/unit/coverage'),
 
-        // preprocessors: {
-        //     'test/unit/*.test.js': [ 'webpack' ]
-        // },
+            // Combines coverage information from multiple browsers into one report rather than outputting a report
+            // for each browser.
+            combineBrowserReports: true,
+
+            // if using webpack and pre-loaders, work around webpack breaking the source path
+            fixWebpackSourcePaths: true,
+
+            // Omit files with no statements, no functions and no branches from the report
+            skipFilesWithNoCoverage: true,
+
+            // Most reporters accept additional config options. You can pass these through the `report-config` option
+            'report-config': {
+                // all options available at: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib/html/index.js#L135-L137
+                html: {
+                    // outputs the report in ./coverage/html
+                    subdir: 'html'
+                }
+            }
+        },
+        htmlReporter: {
+            outputDir: './testReport/unit', // where to put the reports
+            templatePath: null, // set if you moved jasmine_template.html
+            focusOnFailures: true, // reports show failures on start
+            namedFiles: false, // name files instead of creating sub-directories
+            pageTitle: null, // page title for reports; browser info by default
+            urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+            reportName: 'index', // report summary filename; browser info by default
+            // experimental
+            preserveDescribeNesting: false, // folded suites stay folded
+            foldAll: false // reports start folded (only with preserveDescribeNesting)
+        }
     });
 };
--- test/unit/a/index.test.js
+++ test/unit/a/index.test.js
@@ -2,6 +2,6 @@ const demo = require('src/index').a;
 
 describe('add unit test.', function() {
     it('2 + 3 = 5', function() {
-        expect(demo.b()).to.be.equal(1);
+        expect(demo.b()).to.be.equal(2);
     });
 });
